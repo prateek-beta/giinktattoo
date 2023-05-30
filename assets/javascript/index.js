210338105bb74eefc6bdc88d0a55e286a6417955
prateek-beta <60416103+prateek-beta@users.noreply.github.com>
2023-05-30 11:54:31 +0530

    initial commit

---
+++ assets/javascript/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    let date = new Date();
+    let year = date.getFullYear();
+
+    $(".footer").html(`Copyright &copy; GiInk Tattoo
+   ${year} all rights reserved`);
+
+});
